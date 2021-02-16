dfce43ff3734eaa94e8b9d47bd7eda6584d41389
Jackal <jackal@localhost.localdomain>
2021-02-16 14:43:32 -0600

    added basic audio playback

--- quoteBot.py
+++ quoteBot.py
@@ -1,14 +1,30 @@
 #!/bin/python3
 
 import discord
+from discord.ext import commands
 
-client = discord.Client() 
+bot = commands.Bot(command_prefix = '$')
 TOKEN = ""
 with open("TOKEN") as f:
     TOKEN = f.read()
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(bot))
 
-client.run(TOKEN)
+@bot.command()
+async def say(ctx):
+    await play(ctx,"./quotes/test.mp3")
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+async def play(ctx,path):
+    if not ctx.voice_client:
+        vc = await ctx.author.voice.channel.connect()
+    else:
+        vc = ctx.voice_client
+    vc.play(discord.FFmpegPCMAudio(path))
+
+bot.run(TOKEN)
