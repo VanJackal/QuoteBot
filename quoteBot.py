7d9f0e4604fdeb488b582850f06a41f69fa9b166
Jackal <jackal@localhost.localdomain>
2021-02-22 13:02:38 -0600

    added searching of ID

--- quoteBot.py
+++ quoteBot.py
@@ -69,6 +69,11 @@ async def setchannel(ctx):
 @bot.command()
 async def search(ctx,*tags):
     entries = list(db.quotes.find({"tags":{"$in":tags}}))
+    if len(tags) == 1:
+        try:
+            entries.append(db.quotes.find_one({"ID":float(tags[0])}))
+        except:
+            pass
     template = "{:<4} | {:<32} | {:<16}\n"
     result = "```"
     result += template.format("ID","Quote","Author")
